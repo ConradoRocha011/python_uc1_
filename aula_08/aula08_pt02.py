26c99dc0e9eda08049a11c87c96ff138a9d6c23a
Conrado <conradorocha011@gmail.com>
2025-08-12 20:55:49 -0300

    Aula08_pt02 12/08/2025
    Segunda parte da aula

---
+++ aula_08/aula08_pt02.py
@@ -0,0 +1,13 @@
+#aula08_pt02
+
+def main():
+
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite um numero novamente: "))
+    
+    soma = numero1 + numero2
+    
+    print(f"A soma de {numero1} e {numero2} é igual a >>{soma}<<")
+
+if __name__ == "__main__":
+    main()
