39a5e8f9d65cf602a8a7376ce3d48af28fa3e2d5
Conrado <conradorocha011@gmail.com>
2025-08-12 20:34:37 -0300

    Aula_pytho_08 dia12/08/2025

---
+++ aula_08/aula_08.py
@@ -0,0 +1,18 @@
+#Aula_08-atividade 01#
+#Autor:Conrado Rocha#
+#Data: 12/08/2025#
+"""""
+Função Saldacão 
+impremi um texto de boa vindas.
+
+"""
+#def somar_numero(a, b):
+   # return a + b
+
+#numero1 = float(input("Digite um numero: "))
+#numero2 = float(input("Digite novamente um numero: "))
+#resultado = somar_numero(numero1, numero2)
+
+#print(f"O resultado da soma de {numero1} com {numero2} é {resultado}")
+
+
