b61c4c47f03b315834f47ca5c1d3ae7caf769794
Conrado <conradorocha011@gmail.com>
2025-08-12 21:49:49 -0300

    Aula08_pt02 12/08/2025

--- aula_08/aula08_pt02.py
+++ aula_08/aula08_pt02.py
@@ -6,15 +6,31 @@ def saudacao():
 
 if __name__== "__main__":
     saudacao()
- """
+ 
 def main():
 
     numero1 = float(input("Digite o primeiro número: "))
     numero2 = float(input("Digite um numero novamente: "))
     
-    soma = numero1 + numero2
+    #soma = numero1 + numero2
     
     print(f"A soma de {numero1} e {numero2} é igual a !!>>{soma}<<!!")
 
 if __name__ == "__main__":
     main()
+"""
+
+def soma(num1, num2):
+   return num1 + num2
+
+def subtracao(num1, num2):
+   return num1 - num2
+
+def main():
+   resultado_soma = soma(5, 14)
+   resultado_subtracao = subtracao(14, 5)
+   print(f"Soma: >>{resultado_soma}<<")
+   print(f"Subtração: >>{resultado_subtracao}<<")
+
+if __name__ == "__main__":
+   main()
